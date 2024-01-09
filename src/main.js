84e971f36f7be1d422a878634a2f1ded542151bc
rachana pich <pich.rachana19@kit.edu.kh>
2024-01-09 21:05:11 +0700

    creating food recipe application using vue

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).use(store).use(router).mount('#app')
---
+++ src/views/DetailOfRecipeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Detail Page
+    </h1>
+  </div>
+</template>
